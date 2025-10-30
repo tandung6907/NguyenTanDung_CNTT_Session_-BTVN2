58579b7993fef826016e0cc01094908748f489a5
tandung6907 <ntdung692007@gmail.com>
2025-10-30 23:08:38 +0700

    Update NguyenTanDung_B25DTCN081_IT102-K25_Session-07 BTVN09.c

--- NguyenTanDung_B25DTCN081_IT102-K25_Session-07/NguyenTanDung_B25DTCN081_IT102-K25_Session-07 BTVN09.c
+++ NguyenTanDung_B25DTCN081_IT102-K25_Session-07/NguyenTanDung_B25DTCN081_IT102-K25_Session-07 BTVN09.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-	//khai baoo bien n
-	int n,a,b;
-	//cho nguoi dung nhap vao n
-	printf("Enter the positive integer n: ");
-	scanf("%d",&n);
+	//khai baoo bien 
+	int number ,a,b;
+	//cho nguoi dung nhap vao number
+	printf("Enter the positive integer number: ");
+	scanf("%d",&number);
 	//di tim cac cap so
-	for(a=0;pow(a,3)<=n;a++){
-		for(b=0;pow(b,3)<=n;b++){
-			if(pow(a,3)+pow(b,3)==n){
+	for(a=0;pow(a,3)<=number;a++){
+		for(b=0;pow(b,3)<=numbber;b++){
+			if(pow(a,3)+pow(b,3)==number){
 				printf("(%d,%d)\n",a,b);
 				
 			}
@@ -24,3 +24,4 @@ int main(){
 	
 	return 0;
 }
+
